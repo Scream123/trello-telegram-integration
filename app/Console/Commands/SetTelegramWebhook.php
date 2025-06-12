fd303c9babbb218f17aa1354886e114d28287e1c
Volodymyr Marakhovskyi <fly_life@mail.ru>
2025-06-12 19:33:52 +0300

    create command

---
+++ app/Console/Commands/SetTelegramWebhook.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Telegram\Bot\Api;
+use Telegram\Bot\Exceptions\TelegramSDKException;
+
+class SetTelegramWebhook extends Command
+{
+    protected $signature = 'telegram:webhook';
+    protected $description = 'Set the webhook for the Telegram bot';
+    protected $telegram;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->telegram = new Api(config('telegram.bot_token'));
+    }
+
+    public function handle()
+    {
+        $this->info('Setting webhook...');
+
+        try {
+            $response = $this->telegram->setWebhook(['url' => url('api/telegram/webhook')]);
+
+            if ($response) {
+                $this->info('Webhook set successfully.');
+            } else {
+                $this->error('Failed to set webhook. Response: ' . var_export($response, true));
+            }
+        } catch (TelegramSDKException $e) {
+            $this->error('Error: ' . $e->getMessage());
+        }
+    }
+}
