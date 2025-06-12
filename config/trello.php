fd01fa7865f846bca500dd849cd0b65a774abedd
Volodymyr Marakhovskyi <fly_life@mail.ru>
2025-06-12 19:40:34 +0300

    added config for trello

---
+++ config/trello.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'webhook_url' => env('TRELLO_WEBHOOK_URL'),
+    'api_key' => env('TRELLO_API_KEY'),
+    'token' => env('TRELLO_TOKEN'),
+    'id_model' => env('TRELLO_BOARD_ID'),
+    'redirect_uri' => env('TRELLO_REDIRECT_URI'),
+];
