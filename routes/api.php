cbbcf5650a441364aced850465c33191d1d25d0f
Volodymyr Marakhovskyi <fly_life@mail.ru>
2025-06-16 12:03:23 +0300

    added routes api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\TelegramController;
 use App\Http\Controllers\TrelloController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::post('/telegram/webhook', [TelegramController::class, 'handleWebhook']);
 
@@ -10,6 +15,7 @@ Route::post('/send-message', [TelegramController::class, 'sendMessage']);
 Route::post('/telegram/set-webhook', [TelegramController::class, 'setWebhook']);
 
 Route::post('/trello/webhook', [TrelloController::class, 'handleWebhook']);
-
+Route::post('/trello/set-webhook', [TrelloController::class, 'setWebhook']);
 Route::match(['post', 'head'], '/trello/webhook', [TrelloController::class, 'handleWebhook']);
 Route::post('/trello/store-user-data', [TrelloController::class, 'storeUserDataFromToken'])->name('trello.store-user-data');
+
