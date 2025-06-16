0ef6e26ed577b618d3a4abe2a619cbb8d1333d3a
Volodymyr Marakhovskyi <fly_life@mail.ru>
2025-06-16 12:05:17 +0300

    added routes web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TrelloController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/trello/callback', [TrelloController::class, 'callback'])->name('trello.callback');
+
